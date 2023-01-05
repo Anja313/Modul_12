d82bf7c5a1ef980d86905c741ab5b82d2644e4f2
Anja <a-hovhannisyan@web.de>
2023-01-05 15:03:12 +0100

    img-slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Modul_12';
+export class AppComponent implements OnInit{
+ 
+  images = ['9.jpg','10.jpg','14.jpg']
+  currentImage = 0
+  showImage = true;
 
+
+  ngOnInit(){
+ this.updateImage();
+  }
+
+updateImage() {
+
+  setInterval(() => {
+    this.currentImage++;
+    this.currentImage = this.currentImage % this.images.length;
+    this.showImage = false;
   
+
+    setTimeout(() => {
+      this.showImage = true;
+    }, 8);
+  }, 8000)
+  } 
+
 }
